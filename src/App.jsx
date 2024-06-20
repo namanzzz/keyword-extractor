b746f48dd904a19677570469b0786a2ac806da65
Naman Sharma <namansharma50962@gmail.com>
2024-06-20 17:06:19 +0530

    Created Header and Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import {Container, Box} from '@chakra-ui/react'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
-    <Box bg="blue.600" color="white" height="100vh"
+    <Box bg="cyan.400" color="white" height="100vh"
     paddingTop={130}
     >
       <Container maxWidth='3xl' centerContent>
-        <h1>My App</h1>
+        <Header />
+        <Footer />
       </Container>
       
     </Box>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import { Heading,Image,Text } from '@chakra-ui/react'
+import logo from '../assets/light-bulb.svg'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <>
+    <Image src={logo} alt='logo' width={100} marginBottom='1rem'/>
+    <Heading color='white' marginBottom='1rem'>
+        AI Keyword extractor
+    </Heading>
+    <Text fontSize={25} textAlign='center'>
+        Paste The resume text below and we will extract keywords
+    </Text>
+    </>
   )
 }
 
