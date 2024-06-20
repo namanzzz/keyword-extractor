6b8e79ec3439023abaff2e6ed7cf093cccc39490
Naman Sharma <namansharma50962@gmail.com>
2024-06-20 19:17:02 +0530

    API requests

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+
+import {useState} from 'react'
 import React from 'react'
 import {Container, Box} from '@chakra-ui/react'
 import Header from './components/Header'
@@ -5,8 +7,27 @@ import Footer from './components/Footer'
 import TextInput from './components/TextInput'
 
 const App = () => {
+  const [keywords, setKeywords] = useState('');
+  const [isOpen, setIsOpen] = useState(false)
+  const [loading, setLoading] = useState(false);
+
   const extractKeywords = (text) => {
+    setLoading(true)
+    setIsOpen(true)
     console.log(text)
+
+
+    const options = {
+      method: 'POST',
+      headers:{
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${import.meta.env.VITE_OPENAI_API_KEY}`
+      },
+      body: JSON.stringify({
+        model: 'text-davinci-003',
+        prompt: 'Extract keywords from this text. Make the first letter of each word uppercase and seperate with commas'
+      })
+    }
   }
 
 
